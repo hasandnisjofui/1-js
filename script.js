4b22710dc850f5550993947f1e1ea32e328b9a09
hasandnisjofui <miralievhasan347@gmail.com>
2024-02-22 18:09:08 +0500

    looooog

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+var h=prompt("nima qivos botta")
+var d=prompt("odammisiz")
+var f=prompt("hikamtillamisiz")
+var k=prompt("muhammadmisiz")
+var k=prompt("chiqine bottan")
+
+
+console.log(h,d,f,k)
+
